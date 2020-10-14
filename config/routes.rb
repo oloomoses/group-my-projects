b432ee2ca7b02fbea5dcccfbfe195b9b0db27708
oloomoses <olooine@gmail.com>
2020-10-14 19:38:41 +0300

    css style new projects form

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   resources :projects
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+
+  get 'login', to: 'sessions#new'
+  post 'login', to: 'sessions#create'
+  delete 'logout', to: 'sessions#destroy'
 end
